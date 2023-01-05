4345094b8299b14fe9ed299f22afe185087e18cf
Baha <baha@symbol.dev>
2023-01-05 19:13:21 +0300

    initial main tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+# Configure Kubernetes provider and connect to the Kubernetes API server
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "docker-desktop"
+}
+
+# Create an Nginx pod
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    container {
+      image = "nginx:1.23.2"
+      name  = "example"
+    }
+  }
+}
+
+# Create an service
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "terraform-example"
+  }
+  spec {
+    selector = {
+      app = kubernetes_pod.nginx.metadata.0.labels.app
+    }
+    port {
+      port = 80
+    }
+
+    type = "NodePort"
+  }
+
+  depends_on = [
+    kubernetes_pod.nginx
+  ]
+}
